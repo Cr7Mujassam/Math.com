a3cb0feb8f773fc4a11889920562fda4eaf6f8bc
Mujassam Taukir Khan <106069072+Cr7Mujassam@users.noreply.github.com>
2023-02-07 01:40:10 +0530

    Added Prime Function and also prompt added

--- Math.js
+++ Math.js
@@ -1,31 +1,41 @@
 alert("Hello Welcome to Maths Trick")
 
+var p = prompt("Please enter Your Name")
+console.log(p);
+
 
 
 
 function table(){
     var num = document.getElementById("table").value;
     console.log(num);
-    var t1 = num * 1;
-    var t2 = num * 2;
-    var t3 = num * 3;
-    var t4 = num * 4;
-    var t5 = num * 5;
-    var t6 = num * 6;
-    var t7 = num * 7;
-    var t8 = num * 8;
-    var t9 = num * 9;
-    var t10 = num * 10;
+    for(var i=1;i<=10;i++){
+        document.getElementById("p1").innerHTML += num + " * " +  i  + " = " + (num*i)+ '<br>' ;
+    }
 
-    document.getElementById("p1").innerHTML = num + " * 1 = " + t1;
-    document.getElementById("p2").innerHTML = num + " * 2 = " + t2;;
-    document.getElementById("p3").innerHTML = num + " * 3 = " + t3;;
-    document.getElementById("p4").innerHTML = num + " * 4 = " + t4;;
-    document.getElementById("p5").innerHTML = num + " * 5 = " + t5;;
-    document.getElementById("p6").innerHTML = num + " * 6 = " + t6;;
-    document.getElementById("p7").innerHTML = num + " * 7 = " + t7;;
-    document.getElementById("p8").innerHTML = num + " * 8 = " + t8;;
-    document.getElementById("p9").innerHTML = num + " * 9 = " + t9;;
-    document.getElementById("p10").innerHTML = num + " * 10 = " + t10;
+    document.getElementById("name1").innerHTML = " Thank You " + p + " Visit Again.";
+
+}
+
+
+function prime(){
+    var pri = document.getElementById("prime").value;
+    var count = 0;
+    console.log(pri);
+    for(var i=1;i<=pri;i++){
+
+        if(pri%i===0){
+            count++;
+        }
+    }
+
+    if(count===2){
+        document.getElementById("primeAns").innerHTML = "Yes Prime Number";
+    }else{
+        document.getElementById("primeAns").innerHTML = "Not Prime Number";
+    }
+
+
+    document.getElementById("name1").innerHTML = " Thank You " + p + " Visit Again.";
 
 }
